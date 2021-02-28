3203bf5a4e5ed84b7404073e7a35c08680d7cb2d
Antonio Berbel Gonzalez <aberbel@gmail.com>
2021-03-01 00:08:48 +0100

    Sumar primos entre los cincuenta primeros numeros

---
+++ SumarPrimosEntreLosCincuentaPrimerosNumeros.java
@@ -0,0 +1,22 @@
+import java.util.logging.Logger;
+
+public class SumaPrimosEntreLosCincuentaPrimerosNumeros {
+		
+	private static final Logger LOG = Logger.getLogger(SumaPrimosEntreLosCincuentaPrimerosNumeros.class.getName());
+
+	public static void main(String[] args) {
+		int suma=0;
+		for(int i=1;i<=50;i++) {
+			int numerosDivididos=0;
+			for(int y=2;y<=i;y++) {
+				if (i % y == 0) {
+					numerosDivididos++;
+				}
+			}
+			if (numerosDivididos == 1) {
+				suma += i;
+			}
+		}
+		LOG.info("Suma: "+String.valueOf(suma));
+
+	}
